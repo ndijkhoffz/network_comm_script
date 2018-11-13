7a9d6241f9dd9b166b98cb2c29cefbaa0fb3025b
Nicholas Dijkhoffz <Nicholas.Dijkhoffz@sparton.com>
2018-11-13 16:21:26 -0500

    Made changes to send data from a text file. It is still going to a local host but it can now send the data.
    ndijkhoffz

--- project/network_client.py
+++ project/network_client.py
@@ -1,10 +1,18 @@
 import socket
+import os
+
 HOST = '127.0.0.1'
 PORT = 65432
+#dir_path = os.path.dirname(os.path.realpath(__file__))
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
-    data = s.recv(1024)
+    with open("test_file.txt", "r") as f:
+        f.readline()
+        for line in f:
+            b = bytearray(line, "utf-8")
+            s.sendall(b)
+            data = s.recv(1024)
+    f.close
 
 print ('Received', repr(data))
--- project/network_server.py
+++ project/network_server.py
@@ -1,16 +1,20 @@
+# import the socket module
 import socket
 
 HOST = '127.0.0.1'
 PORT = 65432
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    # s is the socket object used to listen for new connections
     s.bind((HOST, PORT))
     s.listen()
     conn, addr = s.accept()
+    # conn is used to send data to the client
     with conn:
         print('Connected by', addr)
         while True:
             data = conn.recv(1024)
+            print(data)
             if not data:
                 break
             conn.sendall(data)
