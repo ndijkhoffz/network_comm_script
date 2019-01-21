062698b4fadc9950dc8d7668e76941fb664f07fb
ndijkhoffz <Nicholas>
2019-01-21 09:53:03 -0500

    Made changes to make it an IPv4 network_comm.

--- project/network_client.py
+++ project/network_client.py
@@ -1,8 +1,9 @@
 import socket
 import os
 
-UDP_IP = "192.168.10.4"
-UDP_PORT = 5005
+UDP_IP = "192.168.10.3"
+UDP_PORT = 50005
+
 #dir_path = os.path.dirname(os.path.realpath(__file__))
 
 MESSAGE = "hello world"
@@ -11,8 +12,8 @@ print "UDP target IP: ", UDP_IP
 print "UDP target port: ", UDP_PORT
 print "message: ", MESSAGE
 
-sock = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
 data = sock.recv(1024)
 
-print ('Received', repr(data))
+print "Received", repr(data)
