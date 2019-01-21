1aef1129232908d96ef34be523a1f96f1185f1dc
ndijkhoffz <Nicholas>
2019-01-21 17:08:57 -0500

    Changed the IP address that was pointed to the target. Also added the bind line to see if I could get back a response from the server.

--- project/network_client.py
+++ project/network_client.py
@@ -1,19 +1,23 @@
 import socket
 import os
 
-UDP_IP = "192.168.10.3"
+UDP_IP = "192.168.10.4"
 UDP_PORT = 50005
 
 #dir_path = os.path.dirname(os.path.realpath(__file__))
 
-MESSAGE = "hello world"
+MESSAGE = "hello world how are you"
 
 print "UDP target IP: ", UDP_IP
 print "UDP target port: ", UDP_PORT
 print "message: ", MESSAGE
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((UDP_IP, UDP_PORT))
+print "Sending message..."
 sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+print "Message sent!"
+print "Waiting for response..."
 data = sock.recv(1024)
 
 print "Received", repr(data)
